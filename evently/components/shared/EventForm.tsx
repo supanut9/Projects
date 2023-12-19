05476ef1f64e2d181495bf66a96c1973c41309c3
Supanut Tansorajprasert <supanut.tanso@gmail.com>
2023-12-19 23:03:02 +0700

    fix: make eventId to be able to be null for Create Event Page

--- evently/components/shared/EventForm.tsx
+++ evently/components/shared/EventForm.tsx
@@ -33,7 +33,7 @@ type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
   event?: IEvent;
-  eventId: string;
+  eventId?: string;
 };
 
 const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
