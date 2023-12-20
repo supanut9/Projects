e8501e733f76b4fe6f8e298e069585f6cda69762
Supanut Tansorajprasert <supanut.tanso@gmail.com>
2023-12-20 12:26:26 +0700

    feat: add BuyTicket feature(UI)

--- evently/app/(root)/events/[id]/page.tsx
+++ evently/app/(root)/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
 import {
   getEventById,
@@ -53,6 +54,7 @@ const EventDetails = async ({
             </div>
 
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
